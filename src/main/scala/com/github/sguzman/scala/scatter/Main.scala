4096b0e2e783bef6ededc73cedc6f865e0a0738e
Salvador Guzman <guzmansalv@gmail.com>
2017-11-23 02:58:28 -0800

    good test of streaming api

--- src/main/scala/com/github/sguzman/scala/scatter/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scatter/Main.scala
@@ -1,14 +1,28 @@
 package com.github.sguzman.scala.scatter
 
+import com.danielasfregola.twitter4s.entities.Tweet
+import com.danielasfregola.twitter4s.entities.streaming.StreamingMessage
 import com.github.sguzman.scala.scatter.jcommander.Args
 import com.github.sguzman.scala.scatter.twitter.Get
 
 object Main {
   def main(args: Array[String]): Unit = {
     val argv = Args(args)
-    val client = twitter.Init(argv)
-    val status = Get.timeline(client)
+    val rest = twitter.Init.rest(argv)
+    val status = Get.timeline(rest)
     val tweets = Get.extract(status.data)
+
+    def printTweetText: PartialFunction[StreamingMessage, Unit] = {
+      case tweet: Tweet => println(tweet.text)
+    }
+
+    val stream = twitter.Init.stream(argv)
+    stream.filterStatuses(follow = Seq(22822722))(printTweetText)
+
     tweets foreach println
+  }
+
+  def printTweetText: PartialFunction[StreamingMessage, Unit] = {
+    case tweet: Tweet => println(tweet.text)
   }
 }
--- src/main/scala/com/github/sguzman/scala/scatter/twitter/Init.scala
+++ src/main/scala/com/github/sguzman/scala/scatter/twitter/Init.scala
@@ -1,11 +1,11 @@
 package com.github.sguzman.scala.scatter.twitter
 
-import com.danielasfregola.twitter4s.TwitterRestClient
+import com.danielasfregola.twitter4s.{TwitterRestClient, TwitterStreamingClient}
 import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken}
 import com.github.sguzman.scala.scatter.jcommander.Args
 
 object Init {
-  def apply(consumerKey: String,
+  def rest(consumerKey: String,
              consumerSecret: String,
              accessToken: String,
              accessSecret: String): TwitterRestClient = {
@@ -16,6 +16,20 @@ object Init {
     client
   }
 
-  def apply(args: Args): TwitterRestClient =
-    apply(args.consumerKey, args.consumerSecret, args.accessToken, args.accessTokenSecret)
+  def rest(args: Args): TwitterRestClient =
+    rest(args.consumerKey, args.consumerSecret, args.accessToken, args.accessTokenSecret)
+
+  def stream(consumerKey: String,
+           consumerSecret: String,
+           accessToken: String,
+           accessSecret: String): TwitterStreamingClient = {
+    val consumer = ConsumerToken(consumerKey, consumerSecret)
+    val access = AccessToken(accessToken, accessSecret)
+    val client = TwitterStreamingClient(consumer, access)
+
+    client
+  }
+
+  def stream(args: Args): TwitterStreamingClient =
+    stream(args.consumerKey, args.consumerSecret, args.accessToken, args.accessTokenSecret)
 }
