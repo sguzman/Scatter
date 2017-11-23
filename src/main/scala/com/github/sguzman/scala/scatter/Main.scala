f291123870eb1af15255d5cfa10d989d8b98a3b4
Salvador Guzman <guzmansalv@gmail.com>
2017-11-23 01:11:42 -0800

    some more cases

--- src/main/scala/com/github/sguzman/scala/scatter/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scatter/Main.scala
@@ -1,13 +1,18 @@
 package com.github.sguzman.scala.scatter
 
-import lol.http._
+import lol.http.{at, _}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main {
   def main(args: Array[String]): Unit = {
     Server.listen(8888) {
-      case GET at "/hello" =>
-        Ok("Hello World!")
+      case GET at url"/hello" =>
+        Ok(s"Hello World")
+      case GET at url"/hello/" =>
+        Ok(s"Hello World")
+      case GET at url"/hello/$id" =>
+        Ok(s"Hello World: $id")
       case _ =>
         NotFound
     }
